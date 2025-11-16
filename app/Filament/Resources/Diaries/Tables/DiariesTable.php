400916ce9f35f938586edaee82c6d768c88b1b43
fazril <fazril@dinamika.ac.id>
2025-11-16 15:05:53 +0700

    feat: library daterangepicker for filter diary at

--- app/Filament/Resources/Diaries/Tables/DiariesTable.php
+++ app/Filament/Resources/Diaries/Tables/DiariesTable.php
@@ -2,15 +2,20 @@
 
 namespace App\Filament\Resources\Diaries\Tables;
 
+use App\Enums\FeelingStatus;
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+use Malzariey\FilamentDaterangepickerFilter\Filters\DateRangeFilter;
 
 class DiariesTable
 {
@@ -20,6 +25,7 @@ class DiariesTable
             ->columns([
                 TextColumn::make('title')
                     ->wrap()
+                    ->searchable()
                     ->lineClamp(2),
                 TextColumn::make('content')
                     ->wrap()
@@ -32,11 +38,20 @@ class DiariesTable
                     ->sortable()
             ])
             ->filters([
-                TrashedFilter::make(),
+                DateRangeFilter::make('diary_at')
+                    ->disableRanges(),
+                SelectFilter::make('feeling_status')
+                    ->options(FeelingStatus::class)
             ])
+            ->filtersTriggerAction(
+                fn (Action $action) => $action
+                    ->button()
+                    ->label('Filter'),
+            )
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make()
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
